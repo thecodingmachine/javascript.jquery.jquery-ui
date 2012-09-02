b5c1603547a342a24375a3a9f8a0bea738d118af
David Négrier <david@davidtcm>
2012-09-02 18:03:03 +0200

    Initial commit

---
+++ install.php
@@ -0,0 +1,37 @@
+<?php
+use Mouf\InstallUtils;
+
+// Let's init Mouf
+InstallUtils::init(InstallUtils::$INIT_APP);
+
+// Let's create the instance
+$moufManager = MoufManager::getMoufManager();
+
+if ($moufManager->instanceExists("jQueryUiLibrary")) {
+	$jQueryUILib = $moufManager->getInstanceDescriptor("jQueryUiLibrary");
+} else {
+	$jQueryUILib = $moufManager->createInstance("WebLibrary");
+	$jQueryUILib->setName("jQueryUiLibrary");
+}
+$jQueryUILib->getProperty("jsFiles")->setValue(array(
+	'vendor/mouf/javascript.jquery.jquery-ui/js/jquery-ui-1.8.20.custom.min.js'
+));
+$jQueryUILib->getProperty("cssFiles")->setValue(array(
+	'vendor/mouf/javascript.jquery.jquery-ui/css/ui-darkness/jquery-ui-1.8.20.custom.css'
+));
+$renderer = $moufManager->getInstanceDescriptor('defaultWebLibraryRenderer');
+$jQueryUILib->getProperty("renderer")->setValue($renderer);
+$jQueryUILib->getProperty("dependencies")->setValue(array($moufManager->getInstanceDescriptor('jQueryLibrary')));
+
+$webLibraryManager = $moufManager->getInstanceDescriptor('defaultWebLibraryManager');
+if ($webLibraryManager) {
+	$libraries = $webLibraryManager->getProperty("webLibraries")->getValue();
+	$libraries[] = $jQueryUILib;
+	$webLibraryManager->getProperty("webLibraries")->setValue($libraries);
+}
+
+// Let's rewrite the MoufComponents.php file to save the component
+$moufManager->rewriteMouf();
+
+// Finally, let's continue the install
+InstallUtils::continueInstall();
