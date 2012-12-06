0e5ff5702e73486a535546cf758c3bde03a09f0f
nguyenk <k.nguyen@thecodingmachine.com>
2012-12-06 10:36:36 +0100

    Updated and fixed install process

--- install.php
+++ install.php
@@ -1,4 +1,6 @@
 <?php
+use Mouf\Html\Utils\WebLibraryManager\WebLibraryInstaller;
+
 require_once __DIR__."/../../autoload.php";
 
 use Mouf\Actions\InstallUtils;
@@ -10,28 +12,12 @@ InstallUtils::init(InstallUtils::$INIT_APP);
 // Let's create the instance
 $moufManager = MoufManager::getMoufManager();
 
-if ($moufManager->instanceExists("jQueryUiLibrary")) {
-	$jQueryUILib = $moufManager->getInstanceDescriptor("jQueryUiLibrary");
-} else {
-	$jQueryUILib = $moufManager->createInstance("\Mouf\Html\Utils\WebLibraryManager\WebLibrary");
-	$jQueryUILib->setName("jQueryUiLibrary");
-}
-$jQueryUILib->getProperty("jsFiles")->setValue(array(
-	'vendor/mouf/javascript.jquery.jquery-ui/js/jquery-ui-1.8.22.custom.min.js'
-));
-$jQueryUILib->getProperty("cssFiles")->setValue(array(
-	'vendor/mouf/javascript.jquery.jquery-ui/css/ui-darkness/jquery-ui.css'
-));
-$renderer = $moufManager->getInstanceDescriptor('defaultWebLibraryRenderer');
-$jQueryUILib->getProperty("renderer")->setValue($renderer);
-$jQueryUILib->getProperty("dependencies")->setValue(array($moufManager->getInstanceDescriptor('jQueryLibrary')));
-
-$webLibraryManager = $moufManager->getInstanceDescriptor('defaultWebLibraryManager');
-if ($webLibraryManager) {
-	$libraries = $webLibraryManager->getProperty("webLibraries")->getValue();
-	$libraries[] = $jQueryUILib;
-	$webLibraryManager->getProperty("webLibraries")->setValue($libraries);
-}
+WebLibraryInstaller::installLibrary("jQueryUiLibrary",
+	array('vendor/mouf/javascript.jquery.jquery-ui/js/jquery-ui-1.8.22.custom.min.js'),
+	array('vendor/mouf/javascript.jquery.jquery-ui/css/ui-darkness/jquery-ui.css'),
+	array('jQueryLibrary'),
+	true
+);
 
 // Let's rewrite the MoufComponents.php file to save the component
 $moufManager->rewriteMouf();
