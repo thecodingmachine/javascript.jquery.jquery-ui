6a1f42c9c98d63ea864dc924eb9a3e0b21e911c1
David Négrier <d.negrier@thecodingmachine.com>
2018-08-28 09:35:57 +0200

    Removing Root_url

--- src/JQueryUIServiceProvider.php
+++ src/JQueryUIServiceProvider.php
@@ -12,12 +12,11 @@ use TheCodingMachine\Funky\ServiceProvider;
 class JQueryUIServiceProvider extends ServiceProvider
 {
     /**
-     * @Factory(name="jQueryFileTreeWebLibrary", tags={@Tag(name="webLibraries", priority=-10.0)})
+     * @Factory(name="jQueryUIWebLibrary", tags={@Tag(name="webLibraries", priority=-10.0)})
      */
     public static function createWebLibrary(ContainerInterface $container): WebLibrary
     {
         return new WebLibrary(array('vendor/mouf/javascript.jquery.jquery-ui/js/jquery-ui-1.10.2.custom.min.js'),
-            array('vendor/mouf/javascript.jquery.jquery-ui/css/ui-lightness/jquery-ui-1.10.2.custom.min.css'),
-            $container->get('root_url'));
+            array('vendor/mouf/javascript.jquery.jquery-ui/css/ui-lightness/jquery-ui-1.10.2.custom.min.css'));
     }
 }
